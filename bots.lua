fb88ea7acf42188c3569a05ed279180984d258d1
EISHU92 <donatto555@gmail.com>
2024-01-09 15:50:57 -0300

    Update bots logic

--- bots.lua
+++ bots.lua
@@ -139,8 +139,97 @@ if bots then
 						ctf.bot_flag_token_up[botname] = nil
 					end
 				end
-			else
-				Logic.OldOnStep(self)
+			else --bas_ctf:red_flag_taken
+				if core.get_node(maps.current_map.teams[team]).name ~= "bas_ctf:"..team.."_flag_taken" then
+					Logic.OldOnStep(self)
+				else
+					-- Find enemy
+					--for team, data in pairs(ctf.team_that_has_bot_to_attack_other) do
+					--	print(dump(data))
+					--	if data and data.bot and data.team == team then
+					--		bots.Hunt(self, data.bot, 1.7)
+					--		core.chat_send_all("hunting "..Name(data.bot))
+					--	end
+					--end
+					for teamA, obj in pairs(ctf.token_flags) do
+						if teamA == team then
+							bots.Hunt(self, obj, 1.8, true)
+							core.chat_send_all("HUNTING "..Name(obj))
+						end
+					end
+					-- Scan enemies in bot view
+					local detected = {}
+					for _, obj in pairs(core.get_objects_inside_radius(self.object:get_pos(), self.view_range+50)) do
+						if Name(obj) and Name(obj) ~= self.bot_name then
+							if obj:get_luaentity() and obj:get_luaentity().bot_name ~= self.bot_name then
+								if bots.is_in_bot_view(self, obj) then
+									if obj:get_luaentity() and obj:get_luaentity().bot_name then
+										if bots.data[obj:get_luaentity().bot_name] and bots.data[self.bot_name] and bots.data[obj:get_luaentity().bot_name].team ~= bots.data[self.bot_name].team then
+											table.insert(detected, obj)
+										end
+									end
+								end
+							elseif obj:is_player() and bs_old.get_player_team_css(obj) ~= "" then
+								if bots.is_in_bot_view(self, obj) then
+									if bs_old.get_player_team_css(obj) ~= bots.data[self.bot_name].team then
+										table.insert(detected, obj)
+									end
+								end
+							end
+						end
+					end
+					-- Shoot to they
+					local name = self.bot_name
+					for _, obj in pairs(detected) do
+						if bots.path_finder_running[self.bot_name] then
+							bots.data[name].object:set_animation(bots.bots_animations[name].walk_mine, bots.bots_animations[name].anispeed, 0)
+						else
+							bots.data[name].object:set_animation(bots.bots_animations[name].mine, bots.bots_animations[name].anispeed, 0)
+						end
+						local to_use = ""
+						local weapon_type = "hand_weapon"
+						if bots.data[name].weapons.hard_weapon ~= "" then
+							to_use = bots.data[name].weapons.hard_weapon
+							weapon_type = "hard_weapon"
+						elseif bots.data[name].weapons.hand_weapon ~= "" then
+							to_use = bots.data[name].weapons.hand_weapon
+							weapon_type = "hand_weapon"
+						end
+						local itemstack = ItemStack(to_use)
+						if itemstack and itemstack ~= "" and itemstack:get_name() ~= "" then
+							if not bots.queue_shot[name] then
+								bots.in_hand_weapon[self.bot_name] = to_use
+								
+								local from = bots.to_2d(self.object:get_pos())
+								local to = bots.to_2d(obj:get_pos())
+								local offset_to = {
+									x = to.x - from.x,
+									y = to.y - from.y
+								}
+								
+								local dir = math.atan2(offset_to.y, offset_to.x) - (math.pi/2)
+								
+								local damage = itemstack:get_definition().RW_gun_capabilities.gun_damage
+								local sound = itemstack:get_definition().RW_gun_capabilities.gun_sound
+								local cooldown = itemstack:get_definition().RW_gun_capabilities.gun_cooldown
+								local velocity = itemstack:get_definition().RW_gun_capabilities.gun_velocity or bots.default_gun_velocity
+								bots.shoot(1, damage or {fleshy=5}, "bs_bots:bullet", sound, velocity, self, obj)
+								if weapon_type == "hand_weapon" then
+									bots.queue_shot[name] = 0.4
+								else
+									bots.queue_shot[name] = cooldown or 0.1
+								end
+								if bots.data[name].wield_item_obj then
+									bots.data[name].wield_item_obj:set_properties({
+										textures = {itemstack:get_name()},
+											visual_size = {x=0.25, y=0.25},
+									})
+								end
+								self.object:set_yaw(dir)
+							end
+						end
+					end
+				end
 			end
 		end
 	end
